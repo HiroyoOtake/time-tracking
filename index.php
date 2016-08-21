1fc90014cda630ecf9364b872663e86ca9cf873c
otake <otake.hiroyo@gmail.com>
2016-08-21 14:47:39 +0900

    START-STOPボタン切り替え

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 //delete
 require_once('functions.php');
 
@@ -7,45 +8,36 @@ $dbh = connectDb();
 // 現在時刻の UNIX timestamp
 $now = time();
 
-// 日付の表示
+// 日時の表示
 $date = date("Y-m-d",$now);
-//var_dump($date);
+$date_and_time = date("Y-m-d H:i:s",$now);
 
-$H = date("H",$now);  //現在時刻の時
-$I = date("i",$now);  //現在時刻の分
-$A = date("i",$now + 60 * 30); //現在時刻の30分後
+// 一週間前の日付を表示
+$date_7days_ago = date("Y-m-d",$now - 60*60*24*7);
 
-$date_old = date("Y-m-d",$now - 60*60*24*7);
+// ボタンの初期状態
+$btn_word = "START";
+$btn_design = "start_btn";
 
-// データを受け取る
 $error = '';
+
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$action = $_POST['action'];
-	$d1 = $_POST['d1'];
-	$g1 = $_POST['g1'];
-	$m1 = $_POST['m1'];
-	$g2 = $_POST['g2'];
-	$m2 = $_POST['m2'];
-	$d2 = $_POST['d2'];
+	// var_dump($_SERVER['REQUEST_METHOD']);
+	@$_SESSION['start_time'] = $_POST['start_time_at_STOP'];
 
-	if ($action == "") {
-		$error = 'アクションを入力して下さい。';
-	} elseif ($d1 == "") {
-		$error = '開始日を入力して下さい。';
-	} elseif ($g1 == "" | $m1 == "") {
-		$error = '開始時刻を入力して下さい。';
-	} elseif ($d2 == "") {
-		$error = '終了日を入力して下さい。';
-	} elseif ($g2 == "" | $m2 == "") {
-		$error = '終了時刻を入力して下さい。';
-	} else {
-		$error = '';
-		
-		$start_time = $d1 . " " . $g1 . ":" . $m1 . ":00";
-		$end_time = $d2 . " " .  $g2 . ":" . $m2 . ":00";
-		$created_at = date("Y-m-d H:i:s");
-		
+	if (isset($_POST['end_time'])) {
+		$start_time = $_SESSION['start_time'];
+		$end_time = $_POST['end_time']; 
+		$created_at = date("Y-m-d H:i:s",$now);
+
+		// var_dump($action);
+		// var_dump($start_time);
+		// var_dump($end_time);
+		// var_dump($created_at);
+
 		$sql = "insert into input_info (action, start_time, end_time, created_at) values (:action, :start_time, :end_time, :created_at)";
 		$stmt = $dbh->prepare($sql);
 
@@ -55,14 +47,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$stmt->bindParam(":created_at", $created_at);
 
 		$stmt->execute();
+
+		$_SESSION['start_time'] = '';
+		unset($_SESSION['start_time']);
+ 	} else {
+		$_SESSION['action'] = $_POST['action'];
+
+		$btn_word = "START";
+		$btn_design = "start_btn";
 		
-		header("Location:index.php");		
-		exit;
+		if ($action == "") {
+			$error = 'アクションを入力して下さい。';
+		} else {
+			$error = '';
+
+			$_SESSION['start_time'] = $_POST['start_time'];
+			$start_time = $_SESSION['start_time'];
+			
+			if (isset($_SESSION['start_time'])) {
+				$btn_word = "STOP";
+				$btn_design = "stop_btn";
+			} 
+		}
 	}
+} else {
+	// STARTを押した後に、別のブラウザで開いた時に必要な処理
+	if (isset($_SESSION['start_time'])) {
+		$start_time = $_SESSION['start_time'];
+		$btn_word = "STOP";
+		$btn_design = "stop_btn";
+	} else {
+		$btn_word = "START";
+		$btn_design = "start_btn";
+	}
+
 }
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -85,28 +104,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		<div class="timer">
 			<div class="form">
 				<form action="" method="post">
-		 
-				<input type="text" name="action" class="action" placeholder="  今日は何をしましたか?">
+				
+				<input type="text" name="action" class="action" placeholder="  今日は何をしましたか?" 
+					<?php if (isset($_SESSION['start_time'])):?>
+						value="<?php echo $_SESSION['action']; ?>"
+					<?php endif ?>
+				>
 
-				<input type="text" name="d1" value="<?php echo $date; ?>" class="d1">
-				<input type="text" name="g1" value="<?php echo sprintf('%02d', $H); ?>" class="d2">
-				:
-				<input type="text" name="m1" value="<?php echo sprintf('%02d', $I); ?>" class="d2">
-				~
-				<input type="text" name="d2" value="<?php echo $date; ?>" class="d1">
-
-				<?php if ($I >= 30): ?> 
-					<?php $H = date("H",strtotime("+1 hour")); ?>
+				<input type="hidden" name="start_time" value="<?php echo $date_and_time; ?>">
+				
+				<?php if(isset($_SESSION['start_time'])): ?>
+				<input type="hidden" name="end_time" value="<?php echo $date_and_time; ?>">
+				<input type="hidden" name="start_time_at_STOP" value="<?php echo $start_time; ?>">
 				<?php endif ?>
 
-				<input type="text" name="g2" value="<?php echo sprintf('%02d', $H); ?>" class="d2">
-				:
-				<input type="text" name="m2" value="<?php echo sprintf('%02d', $A); ?>" class="d2">
-				<input type="submit" name="time-tracking" value="登録" class='btn'>
+				<input type="submit" name="time-tracking" value="<?php echo $btn_word ?>" class="<?php echo $btn_design ?>">
 
 				</form>
 			</div>
 
+			<!-- アクション未入力時のエラーを表示 -->
 			<div class="error">
 				<?php if ($error): ?> 
 				<font color="red">* <?php echo h($error); ?></font><br>
@@ -114,10 +131,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			</div>
 
 			<?php
-			//DBからデータを取り出す
-			$sql = "select * from input_info where start_time > :date_old order by start_time DESC";
+			//一週間前までのデータを表示
+			$sql = "select * from input_info where start_time > :date_7days_ago order by start_time DESC";
 			$stmt = $dbh->prepare($sql);
-			$stmt->bindParam(":date_old", $date_old);
+			$stmt->bindParam(":date_7days_ago", $date_7days_ago);
 			$stmt->execute();
 
 			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -131,12 +148,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			<?php if ($start_date != $prev_date): ?> 
 				<?php $prev_date = $start_date; ?>
 				<ul><li class="date"> <?php echo h($start_date); ?> <?php echo date("D",strtotime($start_date)); ?> </li></ul>
-				<?php endif ?>
+			<?php endif ?>
 										
 			<ul><li class="list"> <?php echo $input['action']; ?> <span class="action_time"> <?php echo substr($input['start_time'],11,5) ?> ~ <?php echo substr($input['end_time'],11,5) ?> </li></ul>
 			<?php endforeach ?>
-			</div)
-
+			</div>
 	</body>
 </html>
 
